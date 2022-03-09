35ee4b16e849a134a99d1707c26b587f230e5688
Robert Irrgang <69185910+rirrgang@users.noreply.github.com>
2022-03-09 15:15:49 +0100

    + Updated settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+SATICFILES_DIR = [os.path.join(BASE_DIR, 'portfolio/static/')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
